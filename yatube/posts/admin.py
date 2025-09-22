f76f1237f779659ea8d6799f7e7d3c77dc683a07
Владас Куодис <vlad250577@yandex.ru>
2025-09-22 12:41:34 +0300

    Add geo to admin.py

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -17,5 +17,6 @@ class PostAdmin(admin.ModelAdmin):
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
+# Russia
 admin.site.register(Post, PostAdmin) 
 admin.site.register(Group)
